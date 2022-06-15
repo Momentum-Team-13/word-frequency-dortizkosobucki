f2bde5146a029d92c2796fbf98dda53606f6d3a5
daniela <d.ortizkosobucki@gmail.com>
2022-06-15 16:22:22 -0400

    no punctuation in progress

--- word_frequency.py
+++ word_frequency.py
@@ -5,10 +5,50 @@ STOP_WORDS = [
 ]
 
 
+def make_lowercase(read_file):
+    lowercase_version = []
+    for word in read_file:
+        lowercase = word.lower()
+        lowercase_version.append(lowercase)
+        # print(lowercase)
+    # print (lowercase_version)
+    return lowercase_version
+
+def remove_punctuations(lowercase_version):
+    for line in lowercase_version:
+        no_period = line.replace(".", "")
+        # no_comma = no_period.remove(",")
+        print (line)
+
+    # no_punctuations = []
+    # expression = [".", ",", "!", "?"]
+    # no_punctuations = l.remove(".")
+    # print (no_punctuations)
+    # for sentence in lowercase_version:
+    #     for 
+    #     no_punctuations.append(lowercase_version)
+    #     print (no_punctuations)
+    #     # expression = [".", ",", "!", "?"]
+    #     # no_punctuations = del [. : , : !]
+    #     # print (no_punctuations)
+
+
+
 def print_word_freq(file):
     """Read in `file` and print out the frequency of words in that file."""
-    pass
+    print (f'Your file is:{file}')
+    with open (file) as open_file:
+            read_file = open_file.readlines()
+    lowercase_version = make_lowercase(read_file)
+    remove_punctuations(lowercase_version)
+    print (lowercase_version)
 
+
+
+# - remove punctuation
+# - normalize all words to lowercase
+# - remove "stop words" -- words used so frequently they are ignored
+# - go through the file word by word and keep a count of how often each word is used
 
 if __name__ == "__main__":
     import argparse
